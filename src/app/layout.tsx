cc23eda12e19b7b58a5905875db5decd0b5f4cb1
Simon Samuel <salviensky@gmail.com>
2025-08-18 13:55:48 +0100

    chore: added plugins for sorting imports and tailwind classes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+import { ThemeProvider } from "@/lib/providers/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
-import { ThemeProvider } from "@/lib/providers/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
